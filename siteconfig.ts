c321af06126a675cc306e69d2a820ef893933b3d
Zakiego <belummikir@gmail.com>
2022-04-04 07:06:44 +0800

    feat: add siteconfig

---
+++ siteconfig.ts
@@ -0,0 +1,35 @@
+export const siteconfig = {
+  socials: [
+    {
+      name: 'Twitter',
+      link: 'https://twitter.com/prasastipagi',
+      display: '@prasastipagi'
+    },
+    {
+      name: 'Twitter',
+      link: 'https://twitter.com/zakiego',
+      display: '@zakiego'
+    },
+    { name: 'Github', link: 'https://github.com/zakiego', display: '@zakiego' },
+    {
+      name: 'Medium',
+      link: 'https://mzakiyuddin.medium.com/',
+      display: 'mzakiyuddin.medium.com'
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/mzakiyuddin',
+      display: 'linkedin.com/in/mzakiyuddin'
+    },
+    {
+      name: 'Spotify',
+      link: 'https://open.spotify.com/show/4H3wqTA5e4YGCbcBF4RX7i',
+      display: 'Prasasti Pagi'
+    },
+    {
+      name: 'Youtube',
+      link: 'https://www.youtube.com/channel/UCta8ym2O2mWFsgnzzIC7TUg/',
+      display: 'Zakiego'
+    }
+  ]
+};
