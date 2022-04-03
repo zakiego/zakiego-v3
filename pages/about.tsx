60dd17b945ca67a61b932454acd598c514e80bce
Zakiego <belummikir@gmail.com>
2022-04-04 07:06:59 +0800

    feat: change socials link

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,8 +4,11 @@ import Image from 'next/image';
 import Container from 'components/Container';
 import avatar from 'public/avatar.jpg';
 import avatarBW from 'public/avatar-bw.jpg';
+import { siteconfig } from '~/siteconfig';
 
 export default function About() {
+  const { socials } = siteconfig;
+
   return (
     <Container title="About – Lee Robinson">
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
@@ -14,25 +17,15 @@ export default function About() {
         </h1>
         <div className="mb-8 prose dark:prose-dark leading-6">
           <h2>Links</h2>
+
           <ul>
-            <li>
-              Twitter: <a href="https://twitter.com/leeerob">@leeerob</a>
-            </li>
-            <li>
-              GitHub: <a href="https://github.com/leerob">@leerob</a>
-            </li>
-            <li>
-              Website:{' '}
-              <Link href="https://leerob.io">
-                <a>https://leerob.io</a>
-              </Link>
-            </li>
-            <li>
-              LinkedIn:{' '}
-              <a href="https://www.linkedin.com/in/leeerob/">
-                https://www.linkedin.com/in/leeerob
-              </a>
-            </li>
+            {socials.map((social) => {
+              return (
+                <li key={social.link}>
+                  {social.name}: <a href={social.link}>{social.display}</a>
+                </li>
+              );
+            })}
           </ul>
           <h2>Bio</h2>
           <h3>Job Title</h3>
