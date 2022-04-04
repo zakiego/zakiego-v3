70947531a9eb82cfc4975149a3b702d786b52cc7
Zakiego <belummikir@gmail.com>
2022-04-04 12:59:12 +0800

    style: lint

--- layouts/snippets.tsx
+++ layouts/snippets.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image';
-
 import Container from 'components/Container';
-import type { PropsWithChildren } from 'react';
 import type { Snippet } from 'contentlayer/generated';
+import Image from 'next/image';
+import type { PropsWithChildren } from 'react';
 
 export default function SnippetLayout({
   children,
@@ -13,10 +12,10 @@ export default function SnippetLayout({
       title={`${snippet.title} - Code Snippet`}
       description="A collection of code snippets – including serverless functions, Node.js scripts, and CSS tricks."
     >
-      <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
-        <div className="flex justify-between w-full mb-8">
+      <article className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
+        <div className="mb-8 flex w-full justify-between">
           <div>
-            <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+            <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
               {snippet.title}
             </h1>
             <p className="text-gray-700 dark:text-gray-300">
@@ -33,7 +32,7 @@ export default function SnippetLayout({
             />
           </div>
         </div>
-        <div className="prose dark:prose-dark w-full">{children}</div>
+        <div className="prose w-full dark:prose-dark">{children}</div>
       </article>
     </Container>
   );
