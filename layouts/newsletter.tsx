ceeff69c43708f84bf3e04258b909ae1804530e0
Zakiego <belummikir@gmail.com>
2022-04-04 12:59:46 +0800

    style: lint

--- layouts/newsletter.tsx
+++ layouts/newsletter.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image';
-import { parseISO, format } from 'date-fns';
-
 import Container from 'components/Container';
 import Subscribe from 'components/Subscribe';
 import type { Newsletter } from 'contentlayer/generated';
+import { format, parseISO } from 'date-fns';
+import Image from 'next/image';
 import type { PropsWithChildren } from 'react';
 
 export default function NewsletterLayout({
@@ -17,11 +16,11 @@ export default function NewsletterLayout({
       date={new Date(newsletter.publishedAt).toISOString()}
       type="article"
     >
-      <article className="flex flex-col items-start justify-center w-full max-w-2xl mx-auto mb-16">
-        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+      <article className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
           {newsletter.title}
         </h1>
-        <div className="flex flex-col items-start justify-between w-full mt-2 md:flex-row md:items-center">
+        <div className="mt-2 flex w-full flex-col items-start justify-between md:flex-row md:items-center">
           <div className="flex items-center">
             <Image
               alt="Lee Robinson"
@@ -35,11 +34,11 @@ export default function NewsletterLayout({
               {format(parseISO(newsletter.publishedAt), 'MMMM dd, yyyy')}
             </p>
           </div>
-          <p className="mt-2 text-sm text-gray-500 min-w-32 md:mt-0">
+          <p className="min-w-32 mt-2 text-sm text-gray-500 md:mt-0">
             {newsletter.readingTime.text}
           </p>
         </div>
-        <div className="w-full prose dark:prose-dark">{children}</div>
+        <div className="prose w-full dark:prose-dark">{children}</div>
         <div className="mt-8">
           <Subscribe />
         </div>
