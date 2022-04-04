cdf4fc0b129bd2557bf9fc5afda1e6b948634b7b
Zakiego <belummikir@gmail.com>
2022-04-04 12:59:37 +0800

    feat: change about page to mdx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,72 +1,22 @@
-/* eslint-disable react/no-unescaped-entities */
-import Container from 'components/Container';
-import Image from 'next/image';
-import Link from 'next/link';
-import avatar from 'public/avatar.jpg';
+import components from 'components/MDXComponents';
+import type { Blog } from 'contentlayer/generated';
+import { allOtherPages } from 'contentlayer/generated';
+import { useMDXComponent } from 'next-contentlayer/hooks';
 
-import { siteconfig } from '~/siteconfig';
+import SimpleLayout from '~/layouts/simple';
 
-export default function About() {
-  const { socials } = siteconfig;
+export default function About({ post }: { post: Blog }) {
+  const Component = useMDXComponent(post.body.code);
 
   return (
-    <Container title={`About – ${siteconfig.profile.name}`}>
-      <div className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
-        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
-          About Me
-        </h1>
-        <div className="prose mb-8 leading-6 dark:prose-dark">
-          <h2>Links</h2>
-
-          <ul>
-            {socials.map((social) => {
-              return (
-                <li key={social.link}>
-                  {social.name}: <a href={social.link}>{social.display}</a>
-                </li>
-              );
-            })}
-          </ul>
-          <h2>Bio</h2>
-          <h3>Job Title</h3>
-          <p>Lee Robinson, Director of Developer Relations at Vercel</p>
-          <h3>Long, 3rd Person</h3>
-          <p>
-            Lee Robinson is the Director of Developer Relations at{' '}
-            <a href="http://vercel.com/">Vercel</a>, where he helps developers
-            build a faster web and leads the Next.js community. Prior to that,
-            Lee was a Senior Software Engineer focused on the frontend. An
-            educator, writer, and speaker, Lee has also created extensive
-            courses on React and Next.js.
-          </p>
-          <h3>Long, 1st Person</h3>
-          <p>
-            Hey, I'm Lee. I lead Developer Relations at{' '}
-            <a href="https://vercel.com/">Vercel</a>, where my team helps
-            developers build a faster web. I'm a Next.js contributor and help
-            lead our open-source communities. I've passionate about frontend
-            development and have created courses on React, Next.js, and web
-            development. I'm an advisor and investor in early stage startups.
-          </p>
-          <h3>Short, 3rd Person</h3>
-          <p>
-            Lee Robinson is the Director of Developer Relations at{' '}
-            <a href="http://vercel.com/">Vercel</a>, where he helps developers
-            build a faster web.
-          </p>
-          <h3>Short, 1st Person</h3>
-          <p>
-            Hey, I'm Lee. I lead Developer Relations at{' '}
-            <a href="https://vercel.com/">Vercel</a>, where my team helps
-            developers build a faster web.
-          </p>
-          <h3>Education</h3>
-          <p>
-            Lee Robinson graduated from Iowa State University with a BS in
-            Computer Engineering.
-          </p>
-        </div>
-      </div>
-    </Container>
+    <SimpleLayout post={post}>
+      <Component components={components as any} />
+    </SimpleLayout>
   );
+}
+
+export async function getStaticProps() {
+  const post = allOtherPages.find((page) => page.slug === 'about');
+
+  return { props: { post } };
 }
