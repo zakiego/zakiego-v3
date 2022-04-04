8e5d1ca686d96dbf505b9509929a037974f95d09
Zakiego <belummikir@gmail.com>
2022-04-04 11:54:26 +0800

    fix: tailwind sort

--- layouts/uses.tsx
+++ layouts/uses.tsx
@@ -6,16 +6,16 @@ export default function UsesLayout({ children }) {
       title="Uses – Lee Robinson"
       description="Here's what tech I'm currently using for coding, videos, and music."
     >
-      <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+      <article className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
           My Gear
         </h1>
-        <p className="text-gray-700 dark:text-gray-300 mt-2 mb-8">
+        <p className="mt-2 mb-8 text-gray-700 dark:text-gray-300">
           Here's what tech I'm currently using for coding, videos, and music.
           Most of these have been accumulated over the past few years, with a
           recent office upgrade in 2020.
         </p>
-        <div className="prose dark:prose-dark w-full">{children}</div>
+        <div className="prose w-full dark:prose-dark">{children}</div>
       </article>
     </Container>
   );
