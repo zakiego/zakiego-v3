5d6f7e3bc23d075f42807671aa9d95043ad0733e
Zakiego <belummikir@gmail.com>
2022-04-04 12:58:36 +0800

    feat: use dynamic value at container

--- components/Container.tsx
+++ components/Container.tsx
@@ -41,7 +41,7 @@ export default function Container(props) {
   const meta = {
     title: `${siteconfig.profile.name} – Developer, writer, creator.`,
     description: `Front-end developer, JavaScript enthusiast, and course creator.`,
-    image: 'https://leerob.io/static/images/banner.png',
+    image: `${siteconfig.url}/static/images/banner.png`,
     type: 'website',
     ...customMeta
   };
@@ -52,15 +52,15 @@ export default function Container(props) {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://leerob.io${router.asPath}`} />
-        <link rel="canonical" href={`https://leerob.io${router.asPath}`} />
+        <meta property="og:url" content={`${siteconfig.url}${router.asPath}`} />
+        <link rel="canonical" href={`${siteconfig.url}${router.asPath}`} />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Lee Robinson" />
+        <meta property="og:site_name" content={siteconfig.profile.name} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@leeerob" />
+        <meta name="twitter:site" content={siteconfig.profile.twitter} />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
