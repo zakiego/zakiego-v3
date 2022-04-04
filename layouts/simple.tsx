7180a6df3e2f807ada3e22ac18be45eaf6a9107b
Zakiego <belummikir@gmail.com>
2022-04-04 13:13:03 +0800

    feat: add simple layout

---
+++ layouts/simple.tsx
@@ -0,0 +1,22 @@
+import Container from 'components/Container';
+import type { Blog } from 'contentlayer/generated';
+import type { PropsWithChildren } from 'react';
+
+export default function SimpleLayout({
+  children,
+  post
+}: PropsWithChildren<{ post: Blog }>) {
+  return (
+    <Container
+      title={`${post.title} – Lee Robinson`}
+      description={post.summary}
+    >
+      <article className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
+          {post.title}
+        </h1>
+        <div className="prose w-full dark:prose-dark">{children}</div>
+      </article>
+    </Container>
+  );
+}
