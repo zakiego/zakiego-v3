18270d01b1340545549dbb7cdb6d55c95a6584f1
Zakiego <belummikir@gmail.com>
2022-04-09 12:15:16 +0800

    feat: change api source for read page

--- pages/api/read.ts
+++ pages/api/read.ts
@@ -7,47 +7,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  type Message = {
-    id: number;
-    link: string;
-    date: string;
-  };
-
-  // const { data, error } = await supabase
-  //   .from<Message>('what_i_read_today')
-  //   .select('id, link, date')
-  //   .order('id', { ascending: false });
-  // // .limit(5);
-
-  // const articles = await Promise.all(
-  //   data.map(async (article) => {
-  //     return {
-  //       ...(await getMetaData(article.link)),
-  //       ...{
-  //         date: new Date(article.date).toLocaleDateString('id-ID', {
-  //           weekday: 'long',
-  //           year: 'numeric',
-  //           month: 'long',
-  //           day: 'numeric',
-  //           hour: 'numeric',
-  //           minute: 'numeric',
-  //           second: 'numeric'
-  //         })
-  //       }
-  //     };
-  //   })
-  // );
-
-  // const fullText = articles.map((article) => {
-  //   return {
-  //     ...article,
-  //     ...{
-  //       fullText: `${article.title} ${article.description} ${article.url} ${article.date}`
-  //     }
-  //   };
-  // });
-
-  const { data } = await fetch('https://v3-beta.zakiego.my.id/api/read').then(
+  const { data } = await fetch('https://v3.zakiego.my.id/api/read').then(
     (resp) => resp.json()
   );
 
