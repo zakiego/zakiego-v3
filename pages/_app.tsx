bccc8b51b79c8278f7f50c7bf1c73ac905988a0a
Zakiego <belummikir@gmail.com>
2022-04-10 18:07:31 +0800

    chore: remove next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,15 @@ import 'styles/global.css';
 
 import { useAnalytics } from 'lib/analytics';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   useAnalytics();
 
   return (
-    <SessionProvider session={pageProps.session}>
-      <ThemeProvider attribute="class">
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
