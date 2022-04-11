d8aa71912559539a7036851193f56f9c19407da6
Zakiego <belummikir@gmail.com>
2022-04-11 08:50:33 +0800

    feat: add project page

---
+++ pages/project.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
+import ReadCard from '~/components/ReadCard';
+import ReadLayout from '~/layouts/read';
+import { siteconfig } from '~/siteconfig';
+import { supabase } from '~/utils/supabaseClient';
+
+export default function Projects() {
+  const { projects } = siteconfig;
+
+  return (
+    <ReadLayout
+      title="Projects"
+      summary="A collection of projects that I work on."
+    >
+      <p>Favorite technology stacks</p>
+      <ul>
+        {siteconfig.techStack.map((tech) => {
+          return (
+            <li key={tech.name}>
+              <a href={tech.href}>{tech.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="my-2 mt-4 grid w-full grid-cols-1 gap-4 ">
+        <h2>Projects</h2>
+        {projects.map((project) => {
+          return (
+            <div
+              key={project.title}
+              className=" w-full rounded-xl   p-4 no-underline transition-colors ease-in-out "
+            >
+              <Image
+                src={project.img}
+                alt={project.title}
+                height={332.2}
+                width={544.5}
+                className="rounded-3xl"
+              />
+              <h3 className="mt-3 text-center">{project.title}</h3>
+              <div className="-mt-4">
+                {project.url && (
+                  <a href={project.url}>
+                    <p className="text-center">{project.url}</p>
+                  </a>
+                )}
+                {!project.url && <p className="text-center italic">private</p>}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </ReadLayout>
+  );
+}
+
+// export async function getStaticProps() {
+//   const { data, error } = await supabase
+//     .from<Project>('projects')
+//     .select()
+//     .order('id', { ascending: false });
+
+//   console.log(data);
+
+//   return { props: { projects: data } };
+// }
--- siteconfig.ts
+++ siteconfig.ts
@@ -7,10 +7,10 @@ export const siteconfig = {
       'Generalist. Fullstack developer. Social science student.',
     twitter: '@prasastipagi'
   },
-  menuMobile: [
+  topNavBar: [
     { display: 'Home', href: '/' },
     { display: 'About', href: '/about' },
-    { display: 'Blog', href: '/blog' },
+    // { display: 'Blog', href: '/blog' },
     // { display: 'Uses', href: 'uses' },
     // { display: 'Snippets', href: '/snippets' },
     { display: 'What I Read Today', href: '/read' },
@@ -39,6 +39,7 @@ export const siteconfig = {
     ],
     rightMenu: [
       { display: 'Uses', href: '/uses' },
+      { display: 'Project', href: '/project' },
       { display: 'What I Read Today', href: '/read' },
       { display: 'How to Work With Me', href: '/readme' }
     ]
@@ -74,6 +75,67 @@ export const siteconfig = {
       name: 'Youtube',
       link: 'https://www.youtube.com/channel/UCta8ym2O2mWFsgnzzIC7TUg/',
       display: 'Zakiego'
+    }
+  ],
+  techStack: [
+    { name: 'Next.js', href: 'https://nextjs.org/' },
+    { name: 'Tailwind CSS', href: 'https://tailwindcss.com/' },
+    { name: 'TypeScript', href: 'https://www.typescriptlang.org/' },
+    { name: 'Vercel', href: 'https://vercel.com/' },
+    { name: 'Prisma', href: 'https://www.prisma.io/' },
+    { name: 'PlanetScale', href: 'https://planetscale.com/' },
+    { name: 'Supabase', href: 'https://supabase.io/' },
+    { name: 'NodeJS', href: 'https://nodejs.org/' }
+  ],
+  projects: [
+    {
+      url: 'https://merawathafalan.my.id',
+      title: 'Merawat Hafalan',
+      description: null,
+      img: 'https://hqxduecuxrozqlaicqkg.supabase.in/storage/v1/object/public/zakiegomyid/merawathafalan.png',
+      private: false
+    },
+    {
+      url: 'https://murottal.my.id',
+      title: 'Kumpulan Murottal',
+      description: null,
+      img: 'https://hqxduecuxrozqlaicqkg.supabase.co/storage/v1/object/public/zakiegomyid/Murottal.png',
+      private: false
+    },
+    {
+      url: 'https://next.zakiego.my.id',
+      title: 'NextJS, TypeScript and Tailwind CSS Starter',
+      description: null,
+      img: 'https://hqxduecuxrozqlaicqkg.supabase.co/storage/v1/object/public/zakiegomyid/ts.png',
+      private: false
+    },
+    {
+      url: 'https://dzikir.vercel.app',
+      title: 'Dzikir Pagi & Sore',
+      description: null,
+      img: 'https://hqxduecuxrozqlaicqkg.supabase.in/storage/v1/object/public/zakiegomyid/dzikir.png',
+      private: false
+    },
+    {
+      url: 'https://kutip.vercel.app',
+      title: 'Kutip Tweet',
+      description: null,
+      img: 'https://hqxduecuxrozqlaicqkg.supabase.in/storage/v1/object/public/zakiegomyid/kutip.png',
+      private: false
+    },
+
+    {
+      url: 'https://pengikut.vercel.app',
+      title: 'Pengikut Pertama',
+      description: null,
+      img: 'https://hqxduecuxrozqlaicqkg.supabase.in/storage/v1/object/public/zakiegomyid/pengikut.png',
+      private: false
+    },
+    {
+      title: 'NIK Reader',
+      description: null,
+      img: 'https://hqxduecuxrozqlaicqkg.supabase.in/storage/v1/object/public/zakiegomyid/nik.png',
+      private: true
     }
   ]
 };
