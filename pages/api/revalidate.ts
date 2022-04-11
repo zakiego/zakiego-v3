9ca8485f4cfd6c49720b794c7ac3ba71cdbf2c7d
Zakiego <belummikir@gmail.com>
2022-04-11 09:03:41 +0800

    feat: add revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,13 +4,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const { path, password } = req.query;
+
+  if (path == undefined || password == undefined) {
+    return res.status(401).json({
+      message: `Check ur query`
+    });
+  }
+
   // Check for secret to confirm this is a valid request
-  if (req.query.password !== process.env.MY_SECRET_TOKEN) {
+  if (password !== process.env.MY_SECRET_TOKEN) {
     return res.status(401).json({ message: 'Invalid password' });
   }
 
   try {
-    await res.unstable_revalidate('/read');
+    await res.unstable_revalidate(`/${path}`);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
