677486001bf8b7efec2575f193dbc23ee2bd3326
Zakiego <belummikir@gmail.com>
2022-04-25 12:05:50 +0800

    feat: remove preact from  next config

--- next.config.js
+++ next.config.js
@@ -14,18 +14,18 @@ module.exports = withContentlayer({
       'hqxduecuxrozqlaicqkg.supabase.in',
       'hqxduecuxrozqlaicqkg.supabase.co'
     ]
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      });
-    }
-
-    return config;
   }
+  // webpack: (config, { dev, isServer }) => {
+  //   // Replace React with Preact only in client production build
+  //   if (!dev && !isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+  //       react: 'preact/compat',
+  //       'react-dom/test-utils': 'preact/test-utils',
+  //       'react-dom': 'preact/compat'
+  //     });
+  //   }
+
+  //   return config;
+  // }
 });
